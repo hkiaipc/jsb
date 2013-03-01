a6df9210916b2cb1101774a442c9f54e8b603e90
hkiaipc <hkiaipc@163.com>
2013-03-01 15:34:05 +0800

    add frmWellItem.cs

---
+++ QuShuiXuKeProject/8.Src/gwlman/gwlman/frmWellItem.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace gwlman
+{
+    partial class frmWellItem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label17 = new System.Windows.Forms.Label();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.btbOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label17
+            // 
+            this.label17.Location = new System.Drawing.Point(5, 170);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(100, 23);
+            this.label17.TabIndex = 12;
+            this.label17.Text = "备注:";
+            this.label17.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textBox17
+            // 
+            this.textBox17.Location = new System.Drawing.Point(111, 167);
+            this.textBox17.Multiline = true;
+            this.textBox17.Name = "textBox17";
+            this.textBox17.Size = new System.Drawing.Size(200, 100);
+            this.textBox17.TabIndex = 13;
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(5, 143);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(100, 23);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "出水流量(m3/s):";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(111, 140);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(200, 21);
+            this.textBox10.TabIndex = 11;
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(5, 116);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(100, 23);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "年开采量(m3):";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(111, 113);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(200, 21);
+            this.textBox11.TabIndex = 9;
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(5, 89);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(100, 23);
+            this.label12.TabIndex = 6;
+            this.label12.Text = "孔径(m):";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(111, 86);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(200, 21);
+            this.textBox12.TabIndex = 7;
+            // 
+            // label13
+            // 
+            this.label13.Location = new System.Drawing.Point(5, 62);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(100, 23);
+            this.label13.TabIndex = 4;
+            this.label13.Text = "凿井深(m):";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(111, 59);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(200, 21);
+            this.textBox13.TabIndex = 5;
+            // 
+            // label14
+            // 
+            this.label14.Location = new System.Drawing.Point(5, 35);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(100, 23);
+            this.label14.TabIndex = 2;
+            this.label14.Text = "水源地点:";
+            this.label14.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(111, 32);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(200, 21);
+            this.textBox14.TabIndex = 3;
+            // 
+            // label15
+            // 
+            this.label15.Location = new System.Drawing.Point(5, 8);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(100, 23);
+            this.label15.TabIndex = 0;
+            this.label15.Text = "水井编号:";
+            this.label15.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(111, 5);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(200, 21);
+            this.textBox15.TabIndex = 1;
+            // 
+            // btbOK
+            // 
+            this.btbOK.Location = new System.Drawing.Point(155, 273);
+            this.btbOK.Name = "btbOK";
+            this.btbOK.Size = new System.Drawing.Size(75, 23);
+            this.btbOK.TabIndex = 14;
+            this.btbOK.Text = "确定";
+            this.btbOK.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(236, 273);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 15;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // frmWellItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 306);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btbOK);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.textBox17);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.textBox15);
+            this.Name = "frmWellItem";
+            this.Text = "水井";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.Button btbOK;
+        private System.Windows.Forms.Button btnCancel;
+
+    }
+}
